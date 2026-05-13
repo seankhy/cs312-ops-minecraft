7890316065649aa136f5fb893e5af939b7297828
Hsun-Yu Kuo <kuohsu@oregonstate.edu>
2026-05-12 18:11:48 -0700

    Pin Minecraft version and set MOTD

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM itzg/minecraft-server:java21
 ENV EULA=TRUE
 ENV MEMORY=1024M
 ENV MOTD="Hsun-Yu Kuo | 934293958"
+ENV VERSION=1.21.4
--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "minecraft" {
 }
 
 resource "aws_instance" "minecraft" {
-  ami                    = "ami-0ec10929233384c7f"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.minecraft.id]
